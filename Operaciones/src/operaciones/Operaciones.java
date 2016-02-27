0dee1751e4188d9880e7cdb272e19e8c2f79b1f9
Rafael Anaya <r.anaya.f@hotmail.com>
2016-02-27 08:50:57 -0500

    actualizacion 2

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -5,6 +5,8 @@
  */
 package operaciones;
 
+import operaciones.OperacionesView.View;
+
 /**
  *
  * @author RAFAEL
@@ -15,7 +17,7 @@ public class Operaciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        View.main(null);
     }
     
 }
